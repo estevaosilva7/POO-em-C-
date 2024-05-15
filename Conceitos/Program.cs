826d6d75c16c3a94df90bbc3190f64c77398ec1e
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-05-15 08:38:30 -0300

    Add files via upload

---
+++ Conceitos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa Pessoa1 = new Pessoa();
+        Pessoa1.nome = "Maria";
+        Pessoa1.idade = 31;
+        Pessoa1.mensagem();
+    }
+}
