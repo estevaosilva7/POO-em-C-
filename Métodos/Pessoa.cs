002dd4ebab2b6ed6d843497c5dcfe472f9999123
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-05-15 08:39:37 -0300

    Add files via upload

---
+++ Métodos/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Métodos 
+{
+    class Pessoa 
+    {
+        public void apresentar()
+        {
+            Console.WriteLine("Olá!!!");
+        }
+
+        public void apresentar(string nome)
+        {
+            Console.WriteLine("Olá "+ nome);
+        }
+
+        public void apresentar (string nome,int idade)
+        {
+            Console.WriteLine("Olá "+nome+ "voceê tem "+idade+" anos!");
+        }
+    }
+
+}
---
+++ Métodos/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+using Métodos; 
+class Program 
+{
+    static void Main(string[] args)
+    {
+        Pessoa obj = new Pessoa();
+        obj.apresentar("Estevão", 25);
+    }
+}
