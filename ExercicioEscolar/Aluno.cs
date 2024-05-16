079bd294fb354c729a2bee0bed95afb418b79221
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-05-16 13:07:39 -0300

    Add files via upload

---
+++ ExercicioEscolar/Aluno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Aluno
+{
+
+    //Atributos
+    public string? nome;
+
+    public double nota1, nota2;
+
+    //Metodos
+
+    public double media()
+    {
+        return(nota1+nota2)/2;
+    }
+
+    public string situacao( double media)
+    {
+        return media >= 7 ? "APROVADO" : "REPROVADO";
+    }
+
+    public void mensagem()
+    {
+
+        double obterMedia = media();
+
+        string obterSituacao = situacao(obterMedia);
+
+        Console.WriteLine(nome+ " está "+obterSituacao+" com média "+obterMedia);
+
+    }
+
+
+}
---
+++ ExercicioEscolar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExercicioEscolar
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+            a.nome = "Estevão";
+            a.nota1 = 7;
+            a.nota2 = 9;
+            a.mensagem();
+
+            Aluno b = new Aluno();
+            b.nome = "Inosuke";
+            b.nota1 = 5;
+            b.nota2 = 3;
+            b.mensagem();
+
+
+        }
+    }
+}
