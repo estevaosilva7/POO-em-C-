e40a7b609dfbe32e8135d487d22851b5d7ce9dc5
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-05-20 19:03:53 -0300

    Add files via upload

---
+++ Acesso/Produto.cs
@@ -0,0 +1,12 @@
+class Produto 
+{
+    // public - Atributos e Métodos visíves em qualquer classe
+
+    // private - Atributos e Métodos visíves apenas na classe que foram criados
+
+    // protected - Atributos e Métodos visíves apenas na classe que foram criados e herdados
+
+    public string? nome;
+
+    private int numero;
+}
---
+++ Acesso/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Acesso
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+
+        {
+            Produto p = new Produto();
+            p.nome = "Teste";
+            // p.numero = 78;
+
+        }
+
+    }
+
+}
