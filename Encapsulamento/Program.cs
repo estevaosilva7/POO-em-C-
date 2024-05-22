d27f1009adfacb18deb06261b63783aa53e0d1f0
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-05-22 08:37:38 -0300

    Add files via upload

---
+++ Encapsulamento/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Aluno a = new Aluno();
+            a.mensagem();
+        }
+    }
+}
