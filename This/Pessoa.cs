951263cb8485b67c30a579210f40239272bd2689
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-05-24 08:28:37 -0300

    Add files via upload

---
+++ This/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Pessoa
+{
+    private string nome = "Tatiana";
+
+    public Pessoa(string nome)
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine(this.nome);
+    }
+}
---
+++ This/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace This
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa("Estevão");
+        }
+    }
+}
