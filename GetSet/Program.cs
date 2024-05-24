c2299bace32edb49484ce9faafe66e81bd883ec2
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-05-24 08:27:47 -0300

    Add files via upload

---
+++ GetSet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Estevão";
+            Console.WriteLine(p.Nome);
+        }
+    }
+}
