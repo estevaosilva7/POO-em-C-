820ce2a623bb811f12c7df0b88ff73ba2d3ee3ac
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-05-25 13:48:06 -0300

    Add files via upload

---
+++ Heranca/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Colaborador obj = new Colaborador("Estevão", 17, 1500);
+        }
+    }
+}
