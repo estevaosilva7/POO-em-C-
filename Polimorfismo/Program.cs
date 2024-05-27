9835aa24836e7b3f4afef30c2df4177cf95ffe02
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-05-27 19:32:59 -0300

    Add files via upload

---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Polimorfismo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Imposto objetoE = new Estagiario();
+            objetoE.valeAlimentacao(1000);
+            objetoE.valeTransporte(1000);
+            Console.WriteLine("-----------------");
+
+            
+            Imposto objetoG = new Gerente();
+            objetoE.valeAlimentacao(5000);
+            objetoE.valeTransporte(5000);
+            Console.WriteLine("-----------------");
+
+            
+            Imposto objetoA = new Atendente();
+            objetoE.valeAlimentacao(2000);
+            objetoE.valeTransporte(2000);
+            Console.WriteLine("-----------------");
+
+        }
+    }
+}
