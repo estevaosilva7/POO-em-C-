9e4728b7b17532a107748dbb94a6976b39c1fe88
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-05-28 09:38:14 -0300

    Add files via upload

---
+++ Estatico/Exemplo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Exemplo
+{
+
+    public static string nome = "Estevão";
+
+    public static void soma(int n1, int n2)
+    {
+        Console.WriteLine(n1+n2);
+
+    }
+}
---
+++ Estatico/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Estatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exemplo.soma(7, 8);
+
+            Console.WriteLine("Olá "+Exemplo.nome);
+        }
+    }
+
+
+}
