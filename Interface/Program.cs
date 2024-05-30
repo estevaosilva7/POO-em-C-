647970531881725dbe1c59cfc3f5da7bf5daa13c
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-05-30 13:00:30 -0300

    Add files via upload

---
+++ Interface/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculo c = new Calculo();
+            c.somar(10,12);
+            c.subtrair(10,12);
+        }
+    }
+    
+}
